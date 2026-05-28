a1aad9edfcaf6543053f70da244f56e9bbb0dc91
Henri-pp09 <notebookdeph@gmail.com>
2026-05-28 17:20:50 -0300

    Primeiro commit feito
    site criado, dois passaros adicionados, 2 audios que precisam ser trocados imediatamente! 0 curiosidades, nota 2/10

---
+++ sexo.js
@@ -0,0 +1,9 @@
+function tocarAudio() {
+    const audio = document.getElementById("meuAudio");
+    audio.play();
+}
+
+function pausarAudio() {
+    const audio = document.getElementById("meuAudio");
+    audio.pause();
+}

f508ce07385e5adfb7064ccb2bf6c5b3a624bd10
Henri-pp09 <notebookdeph@gmail.com>
2026-06-12 15:01:16 -0300

    Mouse adicionado
    Ponteiro é um passáro!!! adicionei ele no catalogo e no hub

--- Site_gabriel/hub.js
+++ Site_gabriel/hub.js
@@ -10,3 +10,18 @@ if (fraseDoDia) {
     const indice = new Date().getDay() % frases.length;
     fraseDoDia.textContent = frases[indice];
 }
+
+const cursor = document.querySelector('.cursor');
+
+    document.addEventListener('mousemove', (e) => {
+  cursor.style.left = e.clientX + 'px';
+  cursor.style.top = e.clientY + 'px';
+    });
+
+    document.addEventListener('mousedown', () => {
+  cursor.style.transform = 'translate(-50%, -50%) scale(1.5)';
+    });
+
+    document.addEventListener('mouseup', () => {
+  cursor.style.transform = 'translate(-50%, -50%) scale(1)';
+    });
